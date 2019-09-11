a52c1e8a2aa481c90a3b9f1c60ad8f15eb9a2733
luwenfeng <wenfeng.lu@vzhilin.com>
2019-09-11 16:43:50 +0800

    初始化

---
+++ pages/info/info.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		<view class="title">
+			{{news.title}}
+		</view>
+		<view class="art-content">
+			<rich-text :nodes="news.content"></rich-text>
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				newsId: "",
+				news: {}
+			};
+		},
+		onLoad(e) {
+			console.log(e)
+			this.newsId = e.newsId
+			this.initialize()
+		},
+		methods: {
+			initialize() {
+				uni.request({
+					url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' + this.newsId,
+					method: 'GET',
+					data: {},
+					success: res => {
+						console.log(res)
+						this.news = res.data
+					},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		padding: 10upx 2%;
+		width: 96%;
+		flex-wrap: warp;
+	}
+
+	.title {
+		line-height: 2em;
+		text-align: center;
+	}
+	.art-content {
+		line-height: 2em;
+	}
+</style>
